5b65efb35cb4cf2f076350098a5103f920d40dc9
Krystofero <80296885+Krystofero@users.noreply.github.com>
2023-01-04 13:19:57 +0100

    Dodanie wielu nowych kolumn do tabeli ofert

--- biuropodrozy/database/migrations/2022_12_05_220922_create_offerts_table.php
+++ biuropodrozy/database/migrations/2022_12_05_220922_create_offerts_table.php
@@ -25,6 +25,23 @@ return new class extends Migration
                 $table->decimal('price', 10, 2);
                 $table->date('startdate');
                 $table->date('enddate');
+                $table->boolean('last_minute')->default(false); //czy oferta jest last minute
+                $table->boolean('promotion')->default(false);
+                $table->decimal('promotion_price', 10, 2)->nullable(); //cena promocyjna
+                $table->decimal('insurance_price', 10, 2)->nullable(); //cena z ubezbieczniem zdrowotnym
+                $table->string('region');
+                $table->string('city');
+                $table->boolean('all_inclusive')->default(false);
+                $table->string('all_inclusive_desc')->nullable(); //opis all inclusive
+                $table->string('place_desc')->nullable(); //opis położenia
+                $table->string('what_in_price')->nullable(); //co w cenie
+                $table->string('hotel_desc')->nullable(); //opis hotelu
+                $table->string('rooms_desc')->nullable(); //opis pokoi
+                $table->integer('people_num'); //ile osób
+                $table->integer('nights_num'); //ile nocy
+                $table->string('disabled_people_desc')->nullable(); //udogodnienia dla niepełnosprawnych
+                $table->string('hotel_email');
+                $table->unsignedBigInteger('hotel_tel');
                 // $table->enum('status', ['Nie rozpoczęto','W trakcie','Zakończono'])->default('Nie rozpoczęto');
                 $table->timestamps();
             });
